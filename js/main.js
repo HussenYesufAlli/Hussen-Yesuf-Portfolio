d457ab6fbfe039ec5f39c3c0d31a5c51585251a4
HussenYesufAlli <hussenyesufalli@gmail.com>
2023-05-26 01:30:38 +0800

    Fix ESLint config

--- js/main.js
+++ js/main.js
@@ -1,19 +1,19 @@
-const menuButton = document.querySelector(".btn-humburger");
-const mobileMenu = document.querySelector(".main-nav-mobile");
-const closeButton = document.querySelector(".close-btn-menu");
-const menuItems = document.querySelectorAll("#menu-list li a");
+const menuButton = document.querySelector('.btn-humburger');
+const mobileMenu = document.querySelector('.main-nav-mobile');
+const closeButton = document.querySelector('.close-btn-menu');
+const menuItems = document.querySelectorAll('#menu-list li a');
 
 const openMenu = () => {
-  mobileMenu.classList.add("open-nav-links");
+  mobileMenu.classList.add('open-nav-links');
 };
 
-menuButton.addEventListener("click", () => openMenu());
-closeButton.addEventListener("click", () => {
-  mobileMenu.classList.remove("open-nav-links");
+menuButton.addEventListener('click', () => openMenu());
+closeButton.addEventListener('click', () => {
+  mobileMenu.classList.remove('open-nav-links');
 });
 
 menuItems.forEach((item) => {
-  item.addEventListener("click", () => {
-    mobileMenu.classList.remove("open-nav-links");
+  item.addEventListener('click', () => {
+    mobileMenu.classList.remove('open-nav-links');
   });
 });
